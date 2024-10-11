3e9e48b2556674a6c3a834c9a545a256d8d8f98b
lmunier <lmunier@protonmail.com>
2024-10-11 12:29:06 +0200

    [Feat] Update pre-commit and add a post-commit rule to manage the automatically stashed submodule.

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -9,8 +9,14 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Copy the pre-commit hook to the .git/hooks directory
-cp "$DIR/hooks/pre-commit" "$DIR/../.git/hooks/pre-commit"
-chmod +x .git/hooks/pre-commit
+# List of hooks to copy
+hooks=("pre-commit" "post-commit")
+
+# Loop through each hook and copy it to the .git/hooks directory
+for hook in "${hooks[@]}"; do
+  cp "$DIR/hooks/$hook" "$DIR/../.git/hooks/$hook"
+  chmod +x "$DIR/../.git/hooks/$hook"
+done
 
 # Update the submodules
 git submodule update --init --recursive
